59db5f4daa0aa12cd5704ee820dfa47f8aa00595
Yesferal Cueva <yesferal@gmail.com>
2025-01-20 22:11:24 -0500

    Add Environment class

---
+++ src/framework/env/env.datasource.js
@@ -0,0 +1,6 @@
+/* Copyright © 2024 Yesferal Cueva. All rights reserved. */
+
+export class Environment {
+    BASE_URL = process.env.REACT_APP_BASE_URL;
+    APP_AUTH = process.env.REACT_APP_AUTH;
+}
