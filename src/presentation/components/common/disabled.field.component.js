24a4b39e65e67988d19ed6c2c55d3f3d9b69a7ba
Yesferal Cueva <yesferal@gmail.com>
2026-02-18 09:24:13 -0500

    Add support to autoGeneratedKey in ReviewForm

--- src/presentation/components/common/disabled.field.component.js
+++ src/presentation/components/common/disabled.field.component.js
@@ -1,6 +1,7 @@
 /* Copyright © 2025 Yesferal Cueva. All rights reserved. */
 
 import { Field } from "formik";
+import { FormGroup, FormLabel } from "react-bootstrap";
 
 export const DisabledFieldComponent = ({
     field, // { name, value, onChange, onBlur }
@@ -8,16 +9,17 @@ export const DisabledFieldComponent = ({
 }) => {
     return (
         <div>
-            <Field name={`${field.name}`}
-                placeholder={`${field.name}`}>
-                {({ field }) => (
-                    <input
-                        type="text"
-                        {...field}
-                        disabled
-                    />
-                )}
-            </Field>
+            <FormGroup>
+                <FormLabel for={`${field.name}`}>{field.name}</FormLabel>
+
+                <Field name={`${field.name}`} type="text"
+                    readOnly
+                    className="blocked-input"
+                    style={{ width: "100%" }} />
+            </FormGroup>
+            <div>
+                &nbsp;
+            </div>
         </div>
     )
 };
